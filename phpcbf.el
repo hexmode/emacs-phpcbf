hexmode/emacs-phpcbf

commit 344922bba19802c2837efa4e85335ea4d1bb208f

Improve handling of errors from call-process and phpcbf
Do all formatting work in a temporary buffer. ‘call-process-region’ will
delete text before trying to run the executable, and we could also fail
later based on exit status. If either case we want to leave the original
buffer unchanged.

As signaling an error is no longer a threat to the main buffer, remove
use of ‘unwind-protect’ (which wasn’t protecting the buffer anyway).

Do not split stdout/stderr. phpcbf returns some error messages (e.g. an
invalid “--standard=…” option) on stdout, and that message should be
captured and provided back to the user (e.g. the confusion in issue #2).

As failures now leave the source buffer unchanged, allow saving to
continue even when phpcbf fails. Present a warning with the failure
message.

Fix quotes within error messages.

phpcbf.el
@@ -72,42 +72,41 @@ falling back to PEAR if none is found."
 (defun phpcbf ()
   "Format the current buffer according to the phpcbf."
   (interactive)
-  (let ((temp-file (make-temp-file "phpcbf"))
-        (now-point (point)))
-    (unwind-protect
-        (let ((status)
-              (stderr)
-              (keep-stderr (list t temp-file)))
+  (let ((point (point))
+        (source (current-buffer))
+        (status) (output))
+    (with-temp-buffer
+      (insert-buffer-substring-no-properties source)
+      (setq status (apply #'call-process-region
+                          (point-min) (point-max)
+                          (phpcbf-executable)
+                          t t nil
+                          (phpcbf--options)))
+      (setq output (buffer-substring-no-properties (point-min) (point-max))))
+    (cond
+     ((equal 1 status)
+      (erase-buffer)
+      (insert output)
+      (goto-char point))
+     ((stringp status)
+      (error "‘phpcbf’ killed by signal %s" status))
+     (t (unless output
+          (setq output "no output"))
+        ;; Strip trailing whitespace
+        (when (string-match "[ \t\n\r]+\\'" output)
+          (setq output (replace-match "" t t output)))
+        (error "‘phpcbf’ failed with code %s: %s" status output)))))
 
-          (setq status
-                (apply #'call-process-region
-		       (point-min) (point-max)
-		       (phpcbf-executable)
-		       t keep-stderr t
-		       (phpcbf--options)))
-
-          (setq stderr
-                (with-temp-buffer
-                  (insert-file-contents temp-file)
-                  (when (> (point-max) (point-min))
-                    (insert ": "))
-                  (buffer-substring-no-properties
-                   (point-min) (line-end-position))))
-
-          (cond
-           ((stringp status)
-            (error "`phpcbf` killed by signal %s%s" status stderr))
-           ((not (equal 1 status))
-            (error "`phpcbf` failed with code %d%s" status stderr))
-           (t (message (format "Formatted to standard '%s'" phpcbf-standard))))
-          ))
-    (delete-file temp-file)
-    (goto-char now-point)))
+(defun phpcbf-warn-on-error ()
+  "Run phpcbf but reduce errors to warnings."
+  (condition-case err
+      (phpcbf)
+    (error err (display-warning 'phpcbf (error-message-string err)))))
 
 ;;;###autoload
 (defun phpcbf-enable-on-save ()
   "Run pbpcbf when this buffer is saved."
-  (add-hook 'before-save-hook 'phpcbf nil t))
+  (add-hook 'before-save-hook 'phpcbf-warn-on-error nil t))
 
 (provide 'phpcbf)
 

commit e6c1d001156e58e569f61356bbbf95450f048ab9

Find the ‘phpcbf’ executable before making the temp buffer
This fixes an edge case originally covered by 16b492e, but then broken
by 344922b, where ‘phpcbf’ may be in a buffer-local ‘exec-path’ that
does not apply to the temporary buffer.

phpcbf.el
@@ -72,14 +72,17 @@ falling back to PEAR if none is found."
 (defun phpcbf ()
   "Format the current buffer according to the phpcbf."
   (interactive)
-  (let ((point (point))
+  (let (;; Make sure to look up the executable before switching to the
+        ;; temp buffer so a buffer-local ‘exec-path’ can be used.
+        (phpcbf (phpcbf-executable))
+        (point (point))
         (source (current-buffer))
         (status) (output))
     (with-temp-buffer
       (insert-buffer-substring-no-properties source)
       (setq status (apply #'call-process-region
                           (point-min) (point-max)
-                          (phpcbf-executable)
+                          phpcbf
                           t t nil
                           (phpcbf--options)))
       (setq output (buffer-substring-no-properties (point-min) (point-max))))